be487ec603f563a1363f3c0e8be587089d0de079
evet8 <evetorrejos@gmail.com>
2022-08-17 10:14:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
-
+// import React from 'react';
+import React, { Component } from "react";
 import { AboutUs, FindUs, Footer, Header, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
-  <div>
+// const App = () => (
+//   <div>
+//     <Navbar />
+//     <Header />
+//     <AboutUs />
+//     <SpecialMenu />
+//     <FindUs />
+//     <Footer />
+//   </div>
+// );
+
+class App extends Component {
+  render() {
+    return   <div>
     <Navbar />
     <Header />
     <AboutUs />
     <SpecialMenu />
     <FindUs />
     <Footer />
-  </div>
-);
+    </div>
+  }
+}
 
 export default App;
